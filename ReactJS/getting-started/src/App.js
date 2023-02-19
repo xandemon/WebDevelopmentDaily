706f417b7ea4e3e9bd3c5c7dc746878d231a18b7
deadclown09 <mesandesh.2014@gmail.com>
2023-02-19 21:58:17 +0545

    Day 25: Started ReactJS - learned the basics

---
+++ ReactJS/getting-started/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+// import { useState } from "react";
+// import { Job } from "./learning concepts/Job";
+// import { planets } from "./learning concepts/planets";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
