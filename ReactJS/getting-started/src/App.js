10cae83d9dcc2248e90897f7c8e9df421c412787
deadclown09 <mesandesh.2014@gmail.com>
2023-02-20 23:40:52 +0545

    Day 26: Learned about hooks, fetch api, react router dom, state management, etc.

--- ReactJS/getting-started/src/App.js
+++ ReactJS/getting-started/src/App.js
@@ -1,10 +1,28 @@
 import "./App.css";
-// import { useState } from "react";
+import { useState } from "react";
 // import { Job } from "./learning concepts/Job";
 // import { planets } from "./learning concepts/planets";
 
 function App() {
-  return <div className="App"></div>;
+  const [excuse, setExcuse] = useState("");
+  function getExcuse(reason) {
+    fetch(`https://excuser-three.vercel.app/v1/excuse/${reason}/`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setExcuse(data[0].excuse);
+      });
+  }
+  return (
+    <div className="App">
+      <h1>Generate An Excuse</h1>
+      <button onClick={() => getExcuse("party")}>Party</button>
+      <button onClick={() => getExcuse("family")}>Family</button>
+      <button onClick={() => getExcuse("office")}>Office</button>
+      <div>{excuse}</div>
+    </div>
+  );
 }
 
 export default App;
